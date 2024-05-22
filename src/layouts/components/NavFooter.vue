57d552fa00823b9f60eccb481720d26a91618fe5
Tan Le <levantan.vn.it@gmail.com>
2024-05-22 15:25:55 +0700

    first commit

---
+++ src/layouts/components/NavFooter.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- footer -->
+  <footer
+    class="flex items-center h-12 mt-5 text-sm bg-gray-100 dark:bg-[#171d30] py-2 z-10 w-full"
+  >
+    <div class="flex flex-1">
+      &copy;&nbsp;
+      {{ new Date().getFullYear() }}&nbsp;
+      Made With&nbsp;
+      <img src="../../assets/images/heart.svg" />&nbsp;
+      By&nbsp;
+      <a
+        class="link text-primary no-underline"
+        href="https://validator247.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        >Validator247</a
+      >
+    </div>
+    <div
+      class="hidden md:!block"
+    >
+      <a
+        class="link text-primary no-underline"
+        href="https://github.com/Validator247"
+        target="noopener noreferrer"
+        >Github</a
+      >
+    </div>
+  </footer>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ff9d00',
+        yes: '#3fb68b',
+        no: '#ff5353',
+        info: '#00b2ff',
+        main: 'var(--text-main)',
+        secondary: 'var(--text-secondary)',
+        active: 'var(--bg-active)',
+      }
+    },
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require('daisyui/src/theming/themes')['[data-theme=light]'],
+          primary: '#666cff',
+        },
+      },
+      {
+        dark: {
+          ...require('daisyui/src/theming/themes')['[data-theme=dark]'],
+          primary: '#666cff',
+          'base-100': '#2a334c',
+          'base-200': '#252d37'
+        },
+      },
+    ],
+  },
+};
